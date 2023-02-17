0eac3191f9cfce653aa6c9f50f78b0596a33fe2f
KWARN KHAM <kwarnkham.moon@gmail.com>
2023-02-17 12:51:16 +0630

    fix FileInput

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -51,36 +51,50 @@ const onRejected = () => {
 
 const updateValue = (files) => {
   if (!files) {
-    emit("update:modelValue", []);
+    emit("update:modelValue", props.multiple ? [] : null);
     return;
   }
-  if (files.length > 0) loading.show();
-  const temp = [];
-  files.forEach((imageFile) => {
-    // console.log("originalFile instanceof Blob", imageFile instanceof Blob);
-    // console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
-    const options = {
-      maxSizeMB: 0.9,
-    };
+  const options = {
+    maxSizeMB: 0.9,
+  };
+  if (props.multiple) {
+    if (files.length > 0) loading.show();
+    const temp = [];
+    files.forEach((imageFile) => {
+      // console.log("originalFile instanceof Blob", imageFile instanceof Blob);
+      // console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
 
-    imageCompression(imageFile, options)
+      imageCompression(imageFile, options)
+        .then(function (compressedFile) {
+          // console.log(
+          //   "compressedFile instanceof Blob",
+          //   compressedFile instanceof Blob
+          // ); // true
+          // console.log(
+          //   `compressedFile size ${compressedFile.size / 1024 / 1024} MB`
+          // ); // smaller than maxSizeMB
+          temp.push(compressedFile);
+          if (temp.length === files.length) {
+            emit("update:modelValue", temp);
+            loading.hide();
+          }
+        })
+        .catch(function (error) {
+          console.log(error.message);
+        });
+    });
+  } else {
+    imageCompression(files, options)
       .then(function (compressedFile) {
-        // console.log(
-        //   "compressedFile instanceof Blob",
-        //   compressedFile instanceof Blob
-        // ); // true
-        // console.log(
-        //   `compressedFile size ${compressedFile.size / 1024 / 1024} MB`
-        // ); // smaller than maxSizeMB
-        temp.push(compressedFile);
-        if (temp.length === files.length) {
-          emit("update:modelValue", temp);
-          loading.hide();
-        }
+        emit(
+          "update:modelValue",
+          new File([compressedFile], compressedFile.name)
+        );
+        loading.hide();
       })
       .catch(function (error) {
         console.log(error.message);
       });
-  });
+  }
 };
 </script>
