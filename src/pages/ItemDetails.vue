525cfdd58795274b5a5c34546b197db898c2a1f1
KWARN KHAM <kwarnkham.moon@gmail.com>
2023-05-12 21:29:54 +0630

    fix puchased qty

--- src/pages/ItemDetails.vue
+++ src/pages/ItemDetails.vue
@@ -5,19 +5,11 @@
     </div>
     <p>{{ item.description }}</p>
     <div class="text-right" v-if="item.type == 1">
-      <div>
-        Total purchased quantity :{{
-          item.purchases.reduce((carry, e) => carry + e.quantity, 0)
-        }}
-      </div>
+      <div>Total purchased quantity :{{ purchasedQty }}</div>
       <div>Total ordered quantity :{{ item.ordered_quantity }}</div>
       <div>Total remaining quantity :{{ item.stock }}</div>
       <div>
-        {{
-          Number(item.stock) +
-          Number(item.ordered_quantity) -
-          item.purchases.reduce((carry, e) => carry + e.quantity, 0)
-        }}
+        {{ Number(item.stock) + Number(item.ordered_quantity) - purchasedQty }}
       </div>
     </div>
     <q-separator spaced />
@@ -26,12 +18,21 @@
 
 <script setup>
 import useUtil from "src/composables/util";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRoute } from "vue-router";
 
 const { api } = useUtil();
 const item = ref(null);
 const route = useRoute();
+
+const purchasedQty = computed(() => {
+  if (!item.value) return 0;
+  else
+    return item.value.purchases.reduce(
+      (carry, e) => carry + (e.status == 1 ? e.quantity : 0),
+      0
+    );
+});
 // const printQR = () => {
 //   dialog({
 //     component: PrintQrDialog,
